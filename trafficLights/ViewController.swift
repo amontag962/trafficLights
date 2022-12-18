071018174937d5d087786a6b7b2508cffa0b140e
Alexander <84538326+amontag962@users.noreply.github.com>
2022-12-18 22:33:33 +0300

    create logic

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yelowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var changeLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+        redLightView.layer.cornerRadius = 50
+        redLightView.alpha = 0.3
+        
+        yelowLightView.layer.cornerRadius = 50
+        yelowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = 50
+        greenLightView.alpha = 0.3
     }
 
-
+    @IBAction func changeLightButtonPressed() {
+        
+        if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yelowLightView.alpha = 1
+        } else if yelowLightView.alpha == 1 {
+            yelowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else {
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        }
+        
+        changeLightButton.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
